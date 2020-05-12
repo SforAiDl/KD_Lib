0cdc2b2291a8bfe9b964670f6380734d019eab0a
Neelay Shah <shahnh19@gmail.com>
2020-05-13 00:04:51 +0530

    Fix import errors

--- KD_Lib/__init__.py
+++ KD_Lib/__init__.py
@@ -5,3 +5,5 @@
 __author__ = """Het Shah"""
 __email__ = 'divhet163@gmail.com'
 __version__ = '0.0.1'
+
+from .original import mnist
--- tests/test_KD_Lib.py
+++ tests/test_KD_Lib.py
@@ -10,27 +10,27 @@ from KD_Lib.KD_Lib.original.mnist import mnist
 
 
 @pytest.fixture
-def generate_numbers():
-    """Sample pytest fixture. Generates list of random integers.
+# def generate_numbers():
+#     """Sample pytest fixture. Generates list of random integers.
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
+#     See more at: http://doc.pytest.org/en/latest/fixture.html
+#     """
 
-    return random.sample(range(100), 10)
-
-
-def test_sum_numbers(generate_numbers):
-    """Sample test function for sum_numbers, using pytest fixture."""
-
-    our_result = KD_Lib.sum_numbers(generate_numbers)
-    assert our_result == sum(generate_numbers)
+#     return random.sample(range(100), 10)
 
 
-def test_max_number(generate_numbers):
-    """Sample test function for max_number, using pytest fixture."""
+# def test_sum_numbers(generate_numbers):
+#     """Sample test function for sum_numbers, using pytest fixture."""
 
-    our_result = KD_Lib.max_number(generate_numbers)
-    assert our_result == max(generate_numbers)
+#     our_result = KD_Lib.sum_numbers(generate_numbers)
+#     assert our_result == sum(generate_numbers)
+
+
+# def test_max_number(generate_numbers):
+#     """Sample test function for max_number, using pytest fixture."""
+
+#     our_result = KD_Lib.max_number(generate_numbers)
+#     assert our_result == max(generate_numbers)
 
 def test_mnist():
     mnist()
