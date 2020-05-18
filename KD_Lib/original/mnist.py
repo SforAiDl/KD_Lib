fc44b77be97c8fae63ba846349d6f31d8063f94a
Khizir Siddiqui <khizirsiddiqui@gmail.com>
2020-05-18 12:52:12 +0530

    add doc string to mnist

--- KD_Lib/original/mnist.py
+++ KD_Lib/original/mnist.py
@@ -12,6 +12,30 @@ from KD_Lib.RKD import RKDLoss
 def mnist(teacher_size=1200, student_size=800, epochs=20, lr=0.01,
           optimizer='SGD', loss='MSE', batch_size=100, distil_weight=0.7,
           temp=20, rkd_angle=0.4, rkd_dist=0.6):
+    """
+    Distill a student linear network (student_size) from a teacher linear
+    network (teacher_size) on MNIST dataset.
+
+    Keyword Arguments:
+        teacher_size {int} -- Size of hidden layer in teacher model
+                              (default: {1200})
+        student_size {int} -- Size of hidden layer in student model
+                              (default: {800})
+        epochs {int} -- Number of epochs to train each model (default: {20})
+        lr {float} -- learning rate (default: {0.01})
+        optimizer {str} -- optimizer to be used (supports SGD and adam)
+                           (default: {'SGD'})
+        loss {str} -- loss function to be used (supports mse, kl and rkd)
+                      (rkd takes two additional arguments rkd_angle and
+                      rkd_dist) (default: {'MSE'})
+        batch_size {int} -- Batch Size to be used for training (default: {100})
+        distil_weight {float} -- ratio of loss function (loss) (default: {0.7})
+        temp {int} -- temperature for distillation (default: {20})
+        rkd_angle {float} -- Applicable only for rkd: ratio of angular
+                             potential difference (default: {0.4})
+        rkd_dist {float} -- Applicable only for rkd: ratio of distance
+                             potential difference (default: {0.6})
+    """
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
