720b7cbbb01c870190cb1aeb6a4e40f8cb5fbefa
avishreekh <avishreekh@gmail.com>
2020-05-25 16:34:55 +0530

    Document RKD and Attention

--- KD_Lib/attention/attention.py
+++ KD_Lib/attention/attention.py
@@ -4,6 +4,25 @@ from KD_Lib.attention import ATLoss
 
 
 class attention(BaseClass):
+    """
+    Implementation of attention-based Knowledge distillation from the paper "Paying More 
+    Attention To The Attention - Improving the Performance of CNNs via Attention Transfer" 
+    https://arxiv.org/pdf/1612.03928.pdf
+
+    :param teacher_model (torch.nn.Module): Teacher model
+    :param student_model (torch.nn.Module): Student model
+    :param train_loader (torch.utils.data.DataLoader): Dataloader for training
+    :param val_loader (torch.utils.data.DataLoader): Dataloader for validation/testing
+    :param optimizer_teacher (torch.optim.*): Optimizer used for training teacher
+    :param optimizer_student (torch.optim.*): Optimizer used for training student
+    :param loss (str): Loss used for training
+    :param temp (float): Temperature parameter for distillation
+    :param distil_weight (float): Weight paramter for distillation loss
+    :param device (str): Device used for training; 'cpu' for cpu and 'cuda' for gpu
+    :param log (bool): True if logging required
+    :param logdir (str): Directory for storing logs
+    """
+    
     def __init__(self, teacher_model, student_model, train_loader, val_loader,
                  optimizer_teacher, optimizer_student, loss='MSE', temp=20.0,
                  distil_weight=0.5, device='cpu', log=False, logdir='./Experiments'):
@@ -25,6 +44,14 @@ class attention(BaseClass):
         self.loss_fn = ATLoss()
 
     def calculate_kd_loss(self, y_pred_student, y_pred_teacher, y_true):
+        """
+        Function used for calculating the KD loss during distillation
+
+        :param y_pred_student (torch.FloatTensor): Prediction made by the student model
+        :param y_pred_teacher (torch.FloatTensor): Prediction made by the teacher model 
+        :param y_true (torch.FloatTensor): Original label
+        """
+        
         soft_student_out = F.softmax(y_pred_student[0]/self.temp, dim=1)
         loss = (1 - self.distil_weight) * F.cross_entropy(soft_student_out,
                                                           y_true)
