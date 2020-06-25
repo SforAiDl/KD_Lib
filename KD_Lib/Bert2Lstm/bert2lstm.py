e367eeb03a3d5358467f9e09cd25d5175fcfa87b
avishreekh <avishreekh@gmail.com>
2020-06-25 14:14:48 +0530

    Re-Format using Black

--- KD_Lib/Bert2Lstm/bert2lstm.py
+++ KD_Lib/Bert2Lstm/bert2lstm.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import (DataLoader, RandomSampler, SequentialSampler, random_split)
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, random_split
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -10,6 +10,7 @@ from KD_Lib.Bert2Lstm.utils import df_to_dataset, batch_to_inputs, set_seed
 
 import numpy as np
 from copy import deepcopy
+
 
 class Bert2LSTM(BaseClass):
     """
@@ -30,17 +31,33 @@ class Bert2LSTM(BaseClass):
     :param log (bool): True if logging required
     :param logdir (str): Directory for storing logs
     """
-    
-    def __init__(self, student_model, distill_train_loader, distill_val_loader, optimizer_student, train_df, val_df,
-                 optimizer_teacher=None,loss_fn='MSE', temp=20.0, distil_weight=0.5, device='cpu', log=False, logdir='./Experiments'):
+
+    def __init__(
+        self,
+        student_model,
+        distill_train_loader,
+        distill_val_loader,
+        optimizer_student,
+        train_df,
+        val_df,
+        optimizer_teacher=None,
+        loss_fn="MSE",
+        temp=20.0,
+        distil_weight=0.5,
+        device="cpu",
+        log=False,
+        logdir="./Experiments",
+    ):
 
         set_seed(42)
 
         self.train_df, self.val_df = train_df, val_df
-        teacher_model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
+        teacher_model = BertForSequenceClassification.from_pretrained(
+            "bert-base-uncased"
+        )
         if optimizer_teacher is None:
             optimizer_teacher = AdamW(teacher_model.parameters())
-        
+
         super(Bert2LSTM, self).__init__(
             teacher_model,
             student_model,
@@ -53,38 +70,46 @@ class Bert2LSTM(BaseClass):
             distil_weight,
             device,
             log,
-            logdir
+            logdir,
         )
-        if self.loss_fn.upper() == 'MSE':
+        if self.loss_fn.upper() == "MSE":
             self.loss_fn = nn.MSELoss()
 
-        elif self.loss_fn.upper() == 'KL':
+        elif self.loss_fn.upper() == "KL":
             self.loss_fn = nn.KLDivLoss()
 
-    def _get_teacher_dataloaders(self, max_seq_length=128, train_batch_size=16, val_batch_size=16, mode='train'):
-        '''
+    def _get_teacher_dataloaders(
+        self, max_seq_length=128, train_batch_size=16, val_batch_size=16, mode="train"
+    ):
+        """
         Helper function for generating dataloaders for the teacher
-        '''
-        bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        
-        if mode == 'validate':
+        """
+        bert_tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+        if mode == "validate":
             val_dataset = df_to_dataset(self.val_df, bert_tokenizer, max_seq_length)
             val_sampler = SequentialSampler(val_dataset)
-            val_loader = DataLoader(val_dataset, sampler=val_sampler, batch_size=val_batch_size) 
-            return val_loader     
+            val_loader = DataLoader(
+                val_dataset, sampler=val_sampler, batch_size=val_batch_size
+            )
+            return val_loader
 
         dataset = df_to_dataset(self.train_df, bert_tokenizer, max_seq_length)
 
-        if mode == 'distill':
+        if mode == "distill":
             distill_sampler = SequentialSampler(dataset)
-            distill_loader = DataLoader(dataset, sampler=distill_sampler, batch_size=train_batch_size)
+            distill_loader = DataLoader(
+                dataset, sampler=distill_sampler, batch_size=train_batch_size
+            )
             return distill_loader
 
-        elif mode == 'train':
+        elif mode == "train":
             train_sampler = RandomSampler(dataset)
-            train_loader = DataLoader(dataset, sampler=train_sampler, batch_size=train_batch_size)
+            train_loader = DataLoader(
+                dataset, sampler=train_sampler, batch_size=train_batch_size
+            )
             return train_loader
-        
+
     def calculate_kd_loss(self, y_pred_student, y_pred_teacher, y_true):
         """
         Function used for calculating the KD loss during distillation
@@ -92,19 +117,24 @@ class Bert2LSTM(BaseClass):
         :param y_pred_teacher (torch.FloatTensor): Prediction made by the teacher model 
         :param y_true (torch.FloatTensor): Original label
         """
-        
-        soft_teacher_out = F.softmax(y_pred_teacher/self.temp, dim=0)
-        soft_student_out = F.softmax(y_pred_student/self.temp, dim=0)
 
-        loss = (1-self.distil_weight) * F.cross_entropy(soft_student_out,
-                                                       y_true)
-        loss += self.distil_weight * self.loss_fn(soft_teacher_out,
-                                                 soft_student_out)
+        soft_teacher_out = F.softmax(y_pred_teacher / self.temp, dim=0)
+        soft_student_out = F.softmax(y_pred_student / self.temp, dim=0)
+
+        loss = (1 - self.distil_weight) * F.cross_entropy(soft_student_out, y_true)
+        loss += self.distil_weight * self.loss_fn(soft_teacher_out, soft_student_out)
         return loss
 
-    def train_teacher(self, epochs=1, plot_losses=True, save_model=True, save_model_pth="./models/teacher.pt",
-        max_seq_length=128, train_batch_size=16):
-        '''
+    def train_teacher(
+        self,
+        epochs=1,
+        plot_losses=True,
+        save_model=True,
+        save_model_pth="./models/teacher.pt",
+        max_seq_length=128,
+        train_batch_size=16,
+    ):
+        """
         Function that will be training the teacher 
         :param epochs (int): Number of epochs you want to train the teacher 
         :param plot_losses (bool): True if you want to plot the losses
@@ -112,8 +142,10 @@ class Bert2LSTM(BaseClass):
         :param save_model_pth (str): Path where you want to store the teacher model
         :param max_seq_length (int): Maximum sequence length paramter for generating dataloaders
         :param train_batch_size (int): Batch size paramter for generating dataloaders
-        '''
-        self.teacher_train_loader = self._get_teacher_dataloaders(max_seq_length, train_batch_size, mode='train')
+        """
+        self.teacher_train_loader = self._get_teacher_dataloaders(
+            max_seq_length, train_batch_size, mode="train"
+        )
 
         self.teacher_model.to(self.device)
         self.teacher_model.train()
@@ -121,8 +153,8 @@ class Bert2LSTM(BaseClass):
         length_of_dataset = len(self.teacher_train_loader.dataset)
         best_acc = 0.0
         self.best_teacher_model_weights = deepcopy(self.teacher_model.state_dict())
-       
-        print('Training Teacher... ')
+
+        print("Training Teacher... ")
 
         for ep in range(epochs):
             epoch_loss = 0.0
@@ -133,7 +165,7 @@ class Bert2LSTM(BaseClass):
                 outputs = self.teacher_model(**inputs)
 
                 loss = outputs[0]
-                out = torch.softmax(outputs[1],dim=1)
+                out = torch.softmax(outputs[1], dim=1)
 
                 pred = out.argmax(dim=1, keepdim=True)
                 correct += pred.eq(batch[3].view_as(pred)).sum().item()
@@ -141,20 +173,22 @@ class Bert2LSTM(BaseClass):
                 self.optimizer_teacher.zero_grad()
                 loss.backward()
                 self.optimizer_teacher.step()
-                
+
                 epoch_loss += loss
 
-            epoch_acc = correct/length_of_dataset
+            epoch_acc = correct / length_of_dataset
             if epoch_acc > best_acc:
                 best_acc = epoch_acc
-                self.best_teacher_model_weights = deepcopy(self.teacher_model.state_dict())
+                self.best_teacher_model_weights = deepcopy(
+                    self.teacher_model.state_dict()
+                )
 
             if self.log:
-                self.writer.add_scalar('Training loss/Teacher', epoch_loss, epochs)
-                self.writer.add_scalar('Training accuracy/Teacher', epoch_acc, epochs)
+                self.writer.add_scalar("Training loss/Teacher", epoch_loss, epochs)
+                self.writer.add_scalar("Training accuracy/Teacher", epoch_acc, epochs)
 
             loss_arr.append(epoch_loss)
-            print(f'Epoch: {ep+1}, Loss: {epoch_loss}, Accuracy: {epoch_acc}')
+            print(f"Epoch: {ep+1}, Loss: {epoch_loss}, Accuracy: {epoch_acc}")
 
         self.teacher_model.load_state_dict(self.best_teacher_model_weights)
         if save_model:
@@ -162,20 +196,26 @@ class Bert2LSTM(BaseClass):
         if plot_losses:
             plt.plot(loss_arr)
 
-    def train_student(self, epochs=10, plot_losses=True, save_model=True, save_model_pth='./models/student.pth'):
-        '''
+    def train_student(
+        self,
+        epochs=10,
+        plot_losses=True,
+        save_model=True,
+        save_model_pth="./models/student.pth",
+    ):
+        """
         Function that will be training the student 
         :param epochs (int): Number of epochs you want to train the teacher 
         :param plot_losses (bool): True if you want to plot the losses
         :param save_model (bool): True if you want to save the student model
         :param save_model_pth (str): Path where you want to save the student model
-        '''
+        """
         self.teacher_model.to(self.device)
         self.teacher_model.eval()
 
         y_pred_teacher = None
 
-        self.teacher_distill_loader = self._get_teacher_dataloaders(mode='distill')
+        self.teacher_distill_loader = self._get_teacher_dataloaders(mode="distill")
 
         for batch in self.teacher_distill_loader:
             batch = tuple(t.to(self.device) for t in batch)
@@ -193,26 +233,26 @@ class Bert2LSTM(BaseClass):
                     y_pred_teacher = np.vstack((y_pred_teacher, logits))
 
         self.student_model.train()
-        
+
         loss_arr = []
         length_of_dataset = len(self.train_loader.dataset)
         best_acc = 0.0
         self.best_student_model_weights = deepcopy(self.student_model.state_dict())
 
-        print('\nTraining student...')
+        print("\nTraining student...")
 
         for ep in range(epochs):
             epoch_loss = 0.0
             correct = 0
 
-            for (data, label), bert_prob in zip(self.train_loader,y_pred_teacher):
+            for (data, label), bert_prob in zip(self.train_loader, y_pred_teacher):
 
                 data = data.to(self.device)
-                teacher_out = bert_prob.to(device) 
+                teacher_out = bert_prob.to(device)
                 label = label.to(self.device)
 
                 student_out = self.student_model(data.t()).squeeze(1)
-                
+
                 loss = self.calculate_kd_loss(student_out, teacher_out, label)
 
                 if isinstance(student_out, tuple):
@@ -230,14 +270,16 @@ class Bert2LSTM(BaseClass):
             epoch_acc = correct / length_of_dataset
             if epoch_acc > best_acc:
                 best_acc = epoch_acc
-                self.best_student_model_weights = deepcopy(self.student_model.state_dict())
+                self.best_student_model_weights = deepcopy(
+                    self.student_model.state_dict()
+                )
 
             if self.log:
-                self.writer.add_scalar('Training loss/Student', epoch_loss, epochs)
-                self.writer.add_scalar('Training accuracy/Student', epoch_acc, epochs)
+                self.writer.add_scalar("Training loss/Student", epoch_loss, epochs)
+                self.writer.add_scalar("Training accuracy/Student", epoch_acc, epochs)
 
             loss_arr.append(epoch_loss)
-            print(f'Epoch: {ep+1}, Loss: {epoch_loss}, Accuracy: {epoch_acc}')
+            print(f"Epoch: {ep+1}, Loss: {epoch_loss}, Accuracy: {epoch_acc}")
 
         self.student_model.load_state_dict(self.best_student_model_weights)
         if save_model:
@@ -249,14 +291,16 @@ class Bert2LSTM(BaseClass):
         return self.evaluate(teacher=False)
 
     def evaluate_teacher(self, max_seq_length=128, val_batch_size=16, verbose=True):
-        self.teacher_val_loader = self._get_teacher_dataloaders(max_seq_length, val_batch_size, mode='validate')
+        self.teacher_val_loader = self._get_teacher_dataloaders(
+            max_seq_length, val_batch_size, mode="validate"
+        )
 
         self.teacher_model.to(self.device)
         self.teacher_model.eval()
 
         correct = 0
         length_of_dataset = len(self.teacher_val_loader.dataset)
-        
+
         for batch in self.teacher_val_loader:
             batch = tuple(t.to(self.device) for t in batch)
             inputs = batch_to_inputs(batch)
--- KD_Lib/Bert2Lstm/utils.py
+++ KD_Lib/Bert2Lstm/utils.py
@@ -8,7 +8,7 @@ from multiprocessing import Pool
 import pandas as pd
 import torch
 from torch.utils.data import TensorDataset
-from torch.utils.data import (DataLoader, RandomSampler, SequentialSampler, random_split)
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, random_split
 from tqdm import tqdm
 import numpy as np
 import random
@@ -55,7 +55,7 @@ def example_to_feature(example_row):
 
     # Account for [CLS] and [SEP] with "- 2"
     if len(tokens_a) > max_seq_length - 2:
-        tokens_a = tokens_a[:(max_seq_length - 2)]
+        tokens_a = tokens_a[: (max_seq_length - 2)]
 
     tokens = tokens_a + [tokenizer.sep_token]
     segment_ids = [0] * len(tokens)
@@ -82,10 +82,12 @@ def example_to_feature(example_row):
 
     label_id = label_map[example.label]
 
-    return InputFeatures(input_ids=input_ids,
-                         input_mask=input_mask,
-                         segment_ids=segment_ids,
-                         label_ids=label_id)
+    return InputFeatures(
+        input_ids=input_ids,
+        input_mask=input_mask,
+        segment_ids=segment_ids,
+        label_ids=label_id,
+    )
 
 
 def features_to_dataset(features):
@@ -98,41 +100,54 @@ def features_to_dataset(features):
     all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
     all_label_ids = torch.tensor([f.label_ids for f in features], dtype=torch.long)
 
-    dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
+    dataset = TensorDataset(
+        all_input_ids, all_input_mask, all_segment_ids, all_label_ids
+    )
     return dataset
 
 
 def df_to_dataset(df, tokenizer, max_seq_length):
-    bert_df = pd.DataFrame({
-        'id': range(len(df)),
-        'label': df.iloc[:,1],
-        'alpha': ['a'] * df.shape[0],
-        'text': df.iloc[:,0].replace(r'\n', ' ', regex=True)
-    })
+    bert_df = pd.DataFrame(
+        {
+            "id": range(len(df)),
+            "label": df.iloc[:, 1],
+            "alpha": ["a"] * df.shape[0],
+            "text": df.iloc[:, 0].replace(r"\n", " ", regex=True),
+        }
+    )
     examples = []
     for (i, line) in enumerate(bert_df.T.to_dict().values()):
-        guid = "%s-%s" % ('train', i)
-        text_a = line['text']
-        label = line['label']
-        examples.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+        guid = "%s-%s" % ("train", i)
+        text_a = line["text"]
+        label = line["label"]
+        examples.append(
+            InputExample(guid=guid, text_a=text_a, text_b=None, label=label)
+        )
 
     label_map = {label: i for i, label in enumerate([0, 1])}
     examples = [(example, label_map, tokenizer, max_seq_length) for example in examples]
 
     with closing(Pool(10)) as p:
-        features = list(tqdm(p.imap(example_to_feature, examples, chunksize=100), total=len(examples)))
+        features = list(
+            tqdm(
+                p.imap(example_to_feature, examples, chunksize=100), total=len(examples)
+            )
+        )
         p.terminate()
 
     return features_to_dataset(features)
 
 
 def batch_to_inputs(batch):
-    inputs = {'input_ids': batch[0],
-              'attention_mask': batch[1],
-              'token_type_ids': batch[2],
-              'labels': batch[3]}
+    inputs = {
+        "input_ids": batch[0],
+        "attention_mask": batch[1],
+        "token_type_ids": batch[2],
+        "labels": batch[3],
+    }
 
     return inputs
+
 
 def set_seed(seed):
     random.seed(seed)
@@ -140,9 +155,10 @@ def set_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
+
 def pad(seq, max_len):
     if len(seq) < max_len:
-        seq = seq + ['<pad>'] * (max_len - len(seq))
+        seq = seq + ["<pad>"] * (max_len - len(seq))
 
     return seq[0:max_len]
 
@@ -150,34 +166,37 @@ def pad(seq, max_len):
 def to_indexes(vocab, words):
     return [vocab.stoi[w] for w in words]
 
+
 def to_dataset(x, y_real):
-        torch_x = torch.tensor(x, dtype=torch.long)
-        # torch_y = torch.tensor(y, dtype=torch.float)
-        torch_real_y = torch.tensor(y_real, dtype=torch.long)
-        return TensorDataset(torch_x, torch_real_y)
+    torch_x = torch.tensor(x, dtype=torch.long)
+    # torch_y = torch.tensor(y, dtype=torch.float)
+    torch_real_y = torch.tensor(y_real, dtype=torch.long)
+    return TensorDataset(torch_x, torch_real_y)
 
 
 def get_essentials(train_df, max_seq_length=128, train_batch_size=16):
-        
-        X, y = train_df.iloc[:,0].values, train_df.iloc[:,1].values
 
-        text_field = data.Field()
-        text_field.build_vocab(X, max_size=10000)
+    X, y = train_df.iloc[:, 0].values, train_df.iloc[:, 1].values
 
-        X_split = [t.split() for t in X]
+    text_field = data.Field()
+    text_field.build_vocab(X, max_size=10000)
 
-        # pad
-        X_pad = [pad(s, max_seq_length) for s in X]
-        
-        # to index
-        X_index = [to_indexes(text_field.vocab, s) for s in X_pad]
-        
-        train_dataset = to_dataset(X_index, y)
-        
-        train_sampler = SequentialSampler(train_dataset)
-        train_loader = DataLoader(train_dataset, sampler=train_sampler, batch_size=train_batch_size,
-                                 drop_last=True)       
+    X_split = [t.split() for t in X]
 
-        return text_field, train_loader
+    # pad
+    X_pad = [pad(s, max_seq_length) for s in X]
 
+    # to index
+    X_index = [to_indexes(text_field.vocab, s) for s in X_pad]
 
+    train_dataset = to_dataset(X_index, y)
+
+    train_sampler = SequentialSampler(train_dataset)
+    train_loader = DataLoader(
+        train_dataset,
+        sampler=train_sampler,
+        batch_size=train_batch_size,
+        drop_last=True,
+    )
+
+    return text_field, train_loader
