847306ac6ef2695591a4f58223c16320375ec9a9
Neelay Shah <shahnh19@gmail.com>
2020-08-18 15:31:51 +0530

    Correct file path

--- tests/test_KD_Lib.py
+++ tests/test_KD_Lib.py
@@ -55,7 +55,7 @@ test_loader = torch.utils.data.DataLoader(
 )
 
 ## BERT to LSTM data
-data_csv = "./KD_Lib/Bert2Lstm/IMDB_Dataset.csv"
+data_csv = "./KD_Lib/BERT2LSTM/IMDB_Dataset.csv"
 df = pd.read_csv(data_csv)
 df["sentiment"].replace({"negative": 0, "positive": 1}, inplace=True)
 
