d060640261fba2496bc9e4ca68ef6fdf3bcfe54f
Neelay Shah <shahnh19@gmail.com>
2020-08-18 23:32:46 +0530

    ...

--- KD_Lib/vanilla/vanilla_kd.py
+++ KD_Lib/vanilla/vanilla_kd.py
@@ -64,13 +64,10 @@ class VanillaKD(BaseClass):
 
         soft_teacher_out = F.softmax(y_pred_teacher / self.temp, dim=1)
         soft_student_out = F.softmax(y_pred_student / self.temp, dim=1)
-<<<<<<< HEAD
-=======
 
         loss = (1 - self.distil_weight) * F.cross_entropy(y_pred_student, y_true)
         loss += (self.distil_weight * self.temp * self.temp) * self.loss_fn(
             soft_teacher_out, soft_student_out
         )
->>>>>>> upstream/master
 
         return loss
