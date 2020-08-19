fd5103b437f1ed0d1856de0b8f8479b965bcf74e
Khizir Siddiqui <khizirsiddiqui@gmail.com>
2020-08-19 11:28:31 +0530

    WIP fix rkd loss metric

--- KD_Lib/KD/vision/RKD/loss_metric.py
+++ KD_Lib/KD/vision/RKD/loss_metric.py
@@ -13,8 +13,10 @@ def pairwaise_distance(output):
     output_squared = output.pow(2).sum(dim=1)
     product = torch.mm(output, output.t())
     result = output_squared.unsqueeze(0) + output_squared.unsqueeze(1) - 2 * product
+    result = result.sqrt()
+    result = result.clone()
     result[range(len(output)), range(len(output))] = 0
-    return result.sqrt()
+    return result
 
 
 class RKDDistanceLoss(nn.Module):
@@ -22,13 +24,37 @@ class RKDDistanceLoss(nn.Module):
     Module for calculating RKD Distance Loss
     """
 
-    def forward(self, teacher, student, normalize=False):
+    def forward(self, teacher, student):
         """
         Forward function
 
         :param teacher (torch.FloatTensor): Prediction made by the teacher model
-        :param student (torch.FloatTensor): Prediction made by the student model 
-        :param normalize (bool): True if inputs need to be normalized 
+        :param student (torch.FloatTensor): Prediction made by the student model
+        """
+
+        with torch.no_grad():
+            t = pairwaise_distance(teacher)
+            t_mean = t[t>0].mean()
+            t = t / (t_mean + 1e-7)
+
+        s = pairwaise_distance(student)
+        s_mean = s[s>0].mean()
+        s = s / (s_mean + 1e-7)
+        return F.smooth_l1_loss(s, t)
+
+
+class RKDAngleLoss(nn.Module):
+    """
+    Module for calculating RKD Angle Loss
+    """
+
+    def forward(self, teacher, student, normalize=True):
+        """
+        Forward function
+
+        :param teacher (torch.FloatTensor): Prediction made by the teacher model
+        :param student (torch.FloatTensor): Prediction made by the student model
+        :param normalize (bool): True if inputs need to be normalized
         """
 
         with torch.no_grad():
@@ -41,32 +67,7 @@ class RKDDistanceLoss(nn.Module):
         if normalize:
             s = F.normalize(s, p=2, dim=2)
         s = torch.bmm(s, s.transpose(1, 2)).view(-1)
-        return F.smooth_l1_loss(s, t, reduction="mean")
-
-
-class RKDAngleLoss(nn.Module):
-    """
-    Module for calculating RKD Angle Loss
-    """
-
-    def forward(self, teacher, student, normalize=False):
-        """
-        Forward function
-
-        :param teacher (torch.FloatTensor): Prediction made by the teacher model
-        :param student (torch.FloatTensor): Prediction made by the student model 
-        :param normalize (bool): True if inputs need to be normalized 
-        """
-
-        with torch.no_grad():
-            t = pairwaise_distance(teacher)
-            if normalize:
-                t = F.normalize(t, p=2, dim=2)
-
-        s = pairwaise_distance(student)
-        if normalize:
-            s = F.normalize(s, p=2, dim=2)
-        return F.smooth_l1_loss(s, t, reduction="mean")
+        return F.smooth_l1_loss(s, t)
 
 
 angle_loss = RKDAngleLoss()
@@ -79,22 +80,24 @@ class RKDLoss(nn.Module):
 
     :param dist_ratio (float): Distance ratio for RKD loss if used
     :param angle_ratio (float): Angle ratio for RKD loss if used
+    :param normalize (boolean): Normalize the input before calculating loss.
     """
 
-    def __init__(self, dist_ratio=0.5, angle_ratio=0.5):
+    def __init__(self, dist_ratio=0.5, angle_ratio=0.5, normalize=True):
         super(RKDLoss, self).__init__()
         self.dist_ratio = dist_ratio
         self.angle_ratio = angle_ratio
+        self.normalize = normalize
 
-    def forward(self, teacher, student, normalize=False):
+    def forward(self, teacher, student):
         """
         Forward function
 
         :param teacher (torch.FloatTensor): Prediction made by the teacher model
-        :param student (torch.FloatTensor): Prediction made by the student model 
-        :param normalize (bool): True if inputs need to be normalized 
+        :param student (torch.FloatTensor): Prediction made by the student model
+        :param normalize (bool): True if inputs need to be normalized
         """
 
-        loss = angle_loss(teacher, student, normalize) * self.angle_ratio
-        loss += distance_loss(teacher, student, normalize) * self.dist_ratio
+        loss = angle_loss(teacher, student, self.normalize) * self.angle_ratio
+        loss += distance_loss(teacher, student) * self.dist_ratio
         return loss
