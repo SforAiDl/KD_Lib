042d088ae4bc5f423b2465a5c5146718465aa4de
Het-Shah <divhet163@gmail.com>
2020-08-23 13:38:06 +0530

    Proper imports

--- KD_Lib/__init__.py
+++ KD_Lib/__init__.py
@@ -5,3 +5,37 @@
 __author__ = """Het Shah"""
 __email__ = "divhet163@gmail.com"
 __version__ = "0.0.3"
+
+from .KD import (
+    VanillaKD,
+    VirtualTeacher,
+    SelfTraining,
+    TAKD,
+    RKDLoss,
+    RCO,
+    NoisyTeacher,
+    SoftRandom,
+    MessyCollab,
+    MeanTeacher,
+    LabelSmoothReg,
+    ProbShift,
+    DML,
+    BANN,
+    Attention,
+)
+
+from .models import (
+    ResNet,
+    ResNet101,
+    ResNet152,
+    ResNet18,
+    ResNet34,
+    ResNet50,
+    resnet_book,
+    LSTMNet,
+    Shallow,
+    NetworkInNetwork,
+    LeNet,
+    ModLeNet,
+)
+
