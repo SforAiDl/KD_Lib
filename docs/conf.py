a73c61dc61290eae1a4fc6430c0893ef1fdd995c
Het Shah <divhet163@gmail.com>
2020-08-24 17:01:35 +0530

    Docs (#69)
    * Add basic docs
    
    * Docs
    
    * Add tutorials
    
    * RTD theme
    
    * Add requirements.txt
    
    * Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -55,3 +55,5 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+# Override readthedocs master_doc
+master_doc = "index"
