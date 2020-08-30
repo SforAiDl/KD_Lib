70f66234886c1c8a9ddbfd9a1091a664e0e2838d
Neelay Shah <shahnh19@gmail.com>
2020-08-30 21:07:56 +0530

    Black

--- KD_Lib/Quantization/dynamic/dynamic_quantization.py
+++ KD_Lib/Quantization/dynamic/dynamic_quantization.py
@@ -12,10 +12,15 @@ class Dynamic_Quantizer(Quantizer):
     """
 
     def __init__(
-        self, model, test_loader, qconfig_spec=None,
+        self,
+        model,
+        test_loader,
+        qconfig_spec=None,
     ):
         super(Dynamic_Quantizer, self).__init__(
-            model, qconfig_spec, test_loader=test_loader,
+            model,
+            qconfig_spec,
+            test_loader=test_loader,
         )
 
     def quantize(self, dtype=torch.qint8, mapping=None):
