d03bebe28d203d6df1d83c157ddbbdede8aadf3c
Neelay Shah <shahnh19@gmail.com>
2020-09-13 15:54:19 +0530

    Fix path

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-with open("requirements_dev.txt", 'r') as file_handle:
+with open("./requirements_dev.txt", 'r') as file_handle:
     # convert file contents into a list
     requirements = file_handle.read().splitlines()
 
