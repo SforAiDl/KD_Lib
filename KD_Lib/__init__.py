dec8446b59f3e2910aac6400a9a5f8d16e645af5
Het-Shah <divhet163@gmail.com>
2021-05-30 12:55:09 +0530

    Bump version: 0.0.3 → 0.0.4

--- KD_Lib/__init__.py
+++ KD_Lib/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Het Shah"""
 __email__ = "divhet163@gmail.com"
-__version__ = "0.0.3"
+__version__ = "__version__ = '0.0.4'"
--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/SforAiDL/KD_Lib",
-    version="0.0.3",
+    version="version='0.0.4'",
     zip_safe=False,
 )
