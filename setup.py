1964596e71d767ef1af6252cf922e13b2591f014
Het-Shah <divhet163@gmail.com>
2021-05-31 20:11:37 +0530

    Bump version: 0.0.20 → 0.0.21

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ def get_requires(path=REQUIRE_PATH):
 if __name__ == "__main__":
     setup(
 	name= NAME,
-    	version='0.0.20',
+    	version='0.0.21',
    	description=DESCRIPTION,
     	long_description=LONG_DESCRIPTION,
     	long_description_content_type="text/markdown",
