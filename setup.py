df095955a06efe5936bd36ee497eddf4e13e0f4a
Het-Shah <divhet163@gmail.com>
2021-05-31 18:56:18 +0530

    Update requirements.txt

--- setup.py
+++ setup.py
@@ -3,53 +3,90 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+import codecs
+import os
 
-with open("README.rst") as readme_file:
-    readme = readme_file.read()
+from setuptools import find_packages, setup
 
-with open("HISTORY.rst") as history_file:
-    history = history_file.read()
+# Basic information
+NAME = "Het Shah"
+DESCRIPTION = "A Pytorch Library to help extend all Knowledge Distillation works"
+VERSION = "0.0.17"
+AUTHOR = "Het Shah"
+EMAIL = "divhet163@gmail.com"
+LICENSE = "MIT"
+REPOSITORY = "https://github.com/SforAiDl/KD_Lib"
+PACKAGE = "KD_Lib"
+with open("README.rst", "r") as f:
+    LONG_DESCRIPTION = f.read()
 
-requirements = [
-    'matplotlib>=3.2.1',
-    'torch>=1.5.0',
-    'torchvision>=0.6.0',
-    'tensorboard>=2.2.1',
-    'torchtext>=0.6.0',
-    'transformers>=0.6.0',
-    'pandas>=1.0.1',
-    'tqdm>=4.42.1',
-    'numpy>=1.18.1'
-]
-
+# Define the keywords
+KEYWORDS = ("Knowledge Distillation", "Pruning", "Quantization", "pytorch", "machine learning", "deep learning")
 setup_requirements = ['pytest-runner']
 
 test_requirements = ['pytest', 'pytest-cov']
 
-setup(
-    author="Het Shah",
-    author_email='divhet163@gmail.com',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+# Define the classifiers
+# See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+CLASSIFIERS = (
+	'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-    ],
-    description="A Pytorch Library to help extend all Knowledge Distillation works",
-    install_requires=requirements,
-    license="MIT license",
-    long_description=readme + '\n\n' + history,
-    include_package_data=True,
-    keywords='KD_Lib',
-    name='KD_Lib',
-    packages=find_packages(include=['KD_Lib']),
-    setup_requires=setup_requirements,
-    test_suite="tests",
-    tests_require=test_requirements,
-    url='https://github.com/SforAiDL/KD_Lib',
-    version='0.0.17',
-    zip_safe=False,
 )
+
+# Important Paths
+PROJECT = os.path.abspath(os.path.dirname(__file__))
+REQUIRE_PATH = "requirements.txt"
+VERSION_PATH = os.path.join(PACKAGE, "version.py")
+PKG_DESCRIBE = "README.rst"
+
+# Directories to ignore in find_packages
+EXCLUDES = ()
+
+
+# helper functions
+def read(*parts):
+    """
+    returns contents of file
+    """
+    with codecs.open(os.path.join(PROJECT, *parts), "rb", "utf-8") as file:
+        return file.read()
+
+
+def get_requires(path=REQUIRE_PATH):
+    """
+    generates requirements from file path given as REQUIRE_PATH
+    """
+    for line in read(path).splitlines():
+        line = line.strip()
+        if line and not line.startswith("#"):
+            yield line
+
+
+# Define the configuration
+CONFIG = {
+    "name": NAME,
+    "version": VERSION,
+    "description": DESCRIPTION,
+    "long_description": LONG_DESCRIPTION,
+    "long_description_content_type": "text/markdown",
+    "classifiers": CLASSIFIERS,
+    "keywords": KEYWORDS,
+    "license": LICENSE,
+    "author": AUTHOR,
+    "author_email": EMAIL,
+    "url": REPOSITORY,
+    "project_urls": {"Source": REPOSITORY},
+    "packages": find_packages(where=PROJECT, exclude=EXCLUDES),
+    "install_requires": list(get_requires()),
+    "python_requires": ">=3.6",
+    "setup_requires": setup_requirements,
+    "test_suite": "tests",
+    "tests_require": test_requirements,
+}
+
+if __name__ == "__main__":
+    setup(**CONFIG)
