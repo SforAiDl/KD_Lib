4545df7c67dfe9a043134f16aa22408ccdc004c4
Het-Shah <divhet163@gmail.com>
2021-05-31 15:45:06 +0530

    Bump version: 0.0.16 → 0.0.17

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url='https://github.com/SforAiDL/KD_Lib',
-    version='0.0.16',
+    version='0.0.17',
     zip_safe=False,
 )
