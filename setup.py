9b02d5c147ddd2294c75b9b5eb1cf5a07161f1fa
Het-Shah <divhet163@gmail.com>
2021-05-31 15:44:49 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,44 +12,44 @@ with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    "matplotlib>=3.2.1",
-    "torch>=1.5.0",
-    "torchvision>=0.6.0",
-    "tensorboard>=2.2.1",
-    "torchtext>=0.6.0",
-    "transformers>=0.6.0",
-    "pandas>=1.0.1",
-    "tqdm>=4.42.1",
-    "numpy>=1.18.1",
+    'matplotlib>=3.2.1',
+    'torch>=1.5.0',
+    'torchvision>=0.6.0',
+    'tensorboard>=2.2.1',
+    'torchtext>=0.6.0',
+    'transformers>=0.6.0',
+    'pandas>=1.0.1',
+    'tqdm>=4.42.1',
+    'numpy>=1.18.1'
 ]
 
-setup_requirements = ["pytest-runner"]
+setup_requirements = ['pytest-runner']
 
-test_requirements = ["pytest", "pytest-cov"]
+test_requirements = ['pytest', 'pytest-cov']
 
 setup(
     author="Het Shah",
-    author_email="divhet163@gmail.com",
+    author_email='divhet163@gmail.com',
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     description="A Pytorch Library to help extend all Knowledge Distillation works",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + "\n\n" + history,
+    long_description=readme + '\n\n' + history,
     include_package_data=True,
-    keywords="KD_Lib",
+    keywords='KD_Lib',
     name='KD_Lib',
-    packages=find_packages(),
+    packages=find_packages(include=['KD_Lib']),
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
-    url="https://github.com/SforAiDL/KD_Lib",
-    version="version='0.0.16'",
+    url='https://github.com/SforAiDL/KD_Lib',
+    version='0.0.16',
     zip_safe=False,
 )
